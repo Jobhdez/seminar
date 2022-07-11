07df6ec9b204aae866e667d0f3fcb9ca250343c8
Job <hj93@protonmail.com>
2022-07-10 21:09:32 -0700

    add view for linear algebra post request.

--- backend/seminar/api/views.py
+++ backend/seminar/api/views.py
@@ -1,8 +1,13 @@
 from arxiv.models import Paper
+from .evaluator import evaluate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PaperSerializer
-
+from rest_framework.parsers import JSONParser
+from .serializers import LinearAlgebraSerializer
+from rest_framework import status
+import json
+from .evaluator import Vec, Matrix
 
 @api_view(['GET'])
 def list_papers(request):
@@ -17,3 +22,25 @@ def list_papers(request):
         serializer = PaperSerializer(papers, many=True)
 
         return Response(serializer.data)
+
+@api_view(['POST'])
+def compute_linear_algebra_expression(request):
+    """
+    Given a Linear Algebra expression this view evaluates it.
+
+    @param request: Http POST request
+    @return: linear algbera expression
+    """
+    serializer = LinearAlgebraSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        data = serializer.data
+        algebra_expr = data['expression']
+        algebra_expr = eval(algebra_expr)
+        algebra_expr = evaluate(algebra_expr)
+
+        return Response({"expression":algebra_expr}, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
